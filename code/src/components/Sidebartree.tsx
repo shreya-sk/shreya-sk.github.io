f40377eb51f439d235eefd75f861979c80b84482
Shreya Kothari <skot0411@uni.sydney.edu.au>
2025-12-15 10:07:22 +1100

    UI fixes

--- code/src/components/Sidebartree.tsx
+++ code/src/components/Sidebartree.tsx
@@ -244,7 +244,7 @@ const SidebarTree = ({ posts, selectedPath, onFileSelect }: SidebarTreeProps) =>
 
   // Calculate dynamic width based on expansion depth
   // Base: 350px (wide pill for full directory name visibility), add ~100px per level of nesting
-  const baseWidth = 300;
+  const baseWidth = 270;
   const widthPerLevel = 100;
   const calculatedWidth = baseWidth + (maxExpandedDepth * widthPerLevel);
 
